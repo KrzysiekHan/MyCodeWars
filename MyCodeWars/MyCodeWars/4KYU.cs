db7c16dc761cee7bbc05840a238f39f18467be33
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-11-14 16:46:11 +0100

    roman converter

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -8,6 +8,39 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Roman Numerals Encoder
+        //Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+        //Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero.
+        //In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 
+        //1666 uses each Roman symbol in descending order: MDCLXVI.
+        //Example:
+        //RomanConvert.Solution(1000) -- should return "M"
+        //Help:
+        //Symbol Value
+        //I          1
+        //V          5
+        //X          10
+        //L          50
+        //C          100
+        //D          500
+        //M          1,000
+        //Remember that there can't be more than 3 identical symbols in a row.
+        public static string Solution(int n)
+        {
+            string roman = "";
+            string[] thousand = { "", "M", "MM", "MMM" };
+            string[] hundred = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] ten = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] one = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            roman += thousand[(n / 1000) % 10];
+            roman += hundred[(n / 100) % 10];
+            roman += ten[(n / 10) % 10];
+            roman += one[n % 10];
+
+            return roman;
+        }
+
         //Write function bmi that calculates body mass index(bmi = weight / height ^ 2).
         //if bmi <= 18.5 return "Underweight"
         //if bmi <= 25.0 return "Normal"
