4d7397da581804e08d03628fff0cb60c6572991e
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-11-18 16:15:55 +0100

    revrot

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -9,6 +9,41 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //The input is a string str of digits.Cut the string into chunks (a chunk here is a substring of the initial string) of 
+        //size sz(ignore the last chunk if its size is less than sz).
+        //If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise 
+        //rotate it to the left by one position.Put together these modified chunks and return the result as a string.
+        //sz is <= 0 or if str is empty return ""
+        //sz is greater(>) than the length of str it is impossible to take a chunk of size sz hence return "".
+        //Examples:
+        //revrot("123456987654", 6) --> "234561876549"
+        //revrot("123456987653", 6) --> "234561356789"
+        //revrot("66443875", 4) --> "44668753"
+        //revrot("66443875", 8) --> "64438756"
+        //revrot("664438769", 8) --> "67834466"
+        //revrot("123456779", 8) --> "23456771"
+        //revrot("", 8) --> ""
+        //revrot("123456779", 0) --> "" 
+        //revrot("563000655734469485", 4) --> "0365065073456944"
+        public static string RevRot(string strng, int sz)
+        {
+            var chunksCount = strng.Length / sz;
+            string response = "";
+            List<string> list = new List<string>();
+            for (int i = 0; i < chunksCount; i++)
+            {
+                list.Add(strng.Substring(i * sz, sz));
+            }
+            foreach (var item in list)
+            {
+                if (item.Sum(x=>Math.Pow(x,3)) % 2 == 0)
+                {
+                    response += item.Reverse();
+                }
+            }
+            return "";
+        }
+
         //Convert string to camel case
         //Complete the method/function so that it converts dash/underscore delimited words into camel casing.
         //The first word within the output should be capitalized only if the original word was 
