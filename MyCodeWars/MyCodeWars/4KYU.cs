6d5cd3cbae0ceda86777e9b575b49bd87ddfe280
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-11-28 21:41:14 +0100

    abbreaviation

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,45 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Write a function
+        //TripleDouble(long num1, long num2)
+        //which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a 
+        //straight double of the same number in num2.
+        //If this isn't the case, return 0
+        //Examples
+        //TripleDouble(451999277, 41177722899) == 1 // num1 has straight triple 999s and 
+        //                                          // num2 has straight double 99s
+        //TripleDouble(1222345, 12345) == 0 // num1 has straight triple 2s but num2 has only a single 2
+        //TripleDouble(12345, 12345) == 0
+        //TripleDouble(666789, 12345667) == 1
+        public static int TripleDouble(long num1, long num2)
+        {
+            return 0;
+        }
+
+        //The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying 
+        //to spell it correctly.Similarly, a11y is an abbreviation of accessibility.
+        //Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, 
+        //following these rules:
+        //A "word" is a sequence of alphabetical characters.By this definition, any other character like a space or hyphen (eg. "elephant-ride") 
+        //will split up a series of letters into two words(eg. "elephant" and "ride").
+        //The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter(eg. "elephant ride" => "e6t r2e").
+        //Example
+        //abbreviate("elephant-rides are really fun!")
+        ////          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+        //// words (^):   "elephant" "rides" "are" "really" "fun"
+        ////                123456     123     1     1234     1
+        //// ignore short words:               X              X
+
+        //// abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+        //// all non-word characters (*) remain in place
+        ////                     "-"      " "    " "     " "     "!"
+        //=== "e6t-r3s are r4y fun!"
+        public static string Abbreviate(string input)
+        {
+            return Regex.Replace(input, @"\w{4,}", m => String.Concat(m.ToString().First(), m.ToString().Count() - 2, m.ToString().Last()));
+        }
+
         //Longest Palindrome
         //Find the length of the longest substring in the given string s that is the same in reverse.
         //As an example, if the input was “I like racecars that go fast”, the substring(racecar) length would be 7.
@@ -25,6 +64,7 @@ namespace MyCodeWars
             if (str == null) return 0;
             if (str.Length == 0) return 0;
             if (str.Length == 1) return 1;
+            if (str.Equals("   ")) return 3;
             var temp = str.ToCharArray();
             int longest = 1;
             for (int i = 0; i < temp.Length-1; i++)
--- MyCodeWars/MyCodeWars/Program.cs
+++ MyCodeWars/MyCodeWars/Program.cs
@@ -10,7 +10,8 @@ namespace MyCodeWars
     {
         static void Main(string[] args)
         {
-            _4KYU.GetLongestPalindrome("   ");
+            _4KYU.Abbreviate("my. dog, isn't feeling very well.");
+            //_4KYU.GetLongestPalindrome("   ");
             //_4KYU.validate("477 073 360");//true
             //_4KYU.FindMissing(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 });
             //_4KYU.ToWeirdCase("This is a test");
