7a3e4cae62405c0a087f2120074a40eda6376dd8
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-12-14 20:56:29 +0100

    two sum

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,32 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Two sum
+        //Write a function that takes an array of numbers(integers for the tests) and a target number.It should 
+        //find two different items in the array that, when added together, give the target value. The indices of these 
+        //items should then be returned in a tuple like so: (index1, index2).
+        //For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+        //The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will
+        //be numbers; target will always be the sum of two different items from that array).
+        //twoSum[1, 2, 3] 4 === (0, 2)
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            List<int> response = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                    {
+                        response.Add(i);
+                        response.Add(j);
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+
         //https://www.codewars.com/kata/checking-groups/train/csharp
         //Checking Groups
         //In English and programming, groups can be made using symbols such as () and { }
