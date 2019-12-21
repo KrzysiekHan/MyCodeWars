a850e3d200184dc1fe228087dd9550d90994abb0
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-12-21 22:11:44 +0100

    meeting

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,25 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+
+        //Meeting
+        //John has invited some friends.His list is:
+        //s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+        //Could you make a program that
+        //makes this string uppercase
+        //gives it sorted in alphabetical order by last name.
+        //When the last names are the same, sort them by first name.Last name and first name 
+        //of a guest come in the result between parentheses separated by a comma.
+        //So the result of function meeting(s) will be:
+        //"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+        //It can happen that in two distinct families with the same family name two people have the same first name too.
+        public static string Meeting(string s)
+        {
+            var format1 = s.Split(';').ToArray().Select(x => x.ToUpper()).ToList();
+            var format2 = format1.Select(x => x.Split(':').ToList());
+            return "response";
+        }
+
         //Length of missing array
         //You get an array of arrays.
         //If you sort the arrays by their length, you will see, that their length-values are consecutive.
