a621ef176cdd6966921938d4fc6a41d158ddd179
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-12-24 17:31:57 +0100

    maze runner position checking

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,83 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Maze runner
+        //You will be given a 2D array of the maze and an array of directions.Your task is to follow the directions given.If you reach the 
+        //end point before all your moves have gone, you should return Finish.If you hit any walls or go outside the maze border, you should 
+        //return Dead.If you find yourself still in the maze after using all the moves, you should return Lost.
+        //The Maze array will look like
+        //maze = [[1,1,1,1,1,1,1],
+        //        [1,0,0,0,0,0,3],
+        //        [1,0,1,0,1,0,1],
+        //        [0,0,1,0,0,0,1],
+        //        [1,0,1,0,1,0,1],
+        //        [1,0,0,0,0,0,1],
+        //        [1,2,1,0,1,0,1]]
+        //..with the following key
+        //0 = Safe place to walk
+        //1 = Wall
+        //2 = Start Point
+        //3 = Finish Point
+        //direction = ["N","N","N","N","N","E","E","E","E","E"] == "Finish"
+        //Rules
+        //1. The Maze array will always be square i.e.N x N but its size and content will alter from test to test.
+        //2. The start and finish positions will change for the final tests.
+        //3. The directions array will always be in upper case and will be in the format of N = North, E = East, W = West and S = South.
+        public static string mazeRunner(int[,] maze, string[] directions)
+        {
+            int currX = 0, currY = 0;
+            FieldResult temporaryResult = FieldResult.Start;
+            foreach (var item in directions)
+            {
+                switch (item)
+                {
+                    case "N":
+                        currY++;
+                        temporaryResult = checkMazePosiion(maze[currY, currX]);
+                        break;
+                    case "E":
+                        currX++;
+                        temporaryResult = checkMazePosiion(maze[currY, currX]);
+                        break;
+                    case "W":
+                        currX--;
+                        temporaryResult = checkMazePosiion(maze[currY, currX]);
+                        break;
+                    case "S":
+                        currY--;
+                        temporaryResult = checkMazePosiion(maze[currY, currX]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return "Lost";
+        }
+        private enum FieldResult
+        {
+            Continue,
+            Start,
+            Finish,
+            Dead,
+            Lost
+        }
+        private static FieldResult checkMazePosiion (int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    return FieldResult.Continue;
+                case 1:
+                    return FieldResult.Dead;
+                case 2:
+                    return FieldResult.Start;
+                case 3:
+                    return FieldResult.Finish;
+            }
+            return FieldResult.Lost;
+        }
+
+
         //Fibonacci, Tribonacci and friends
         //If you have completed the Tribonacci sequence kata, you would know by now that mister Fibonacci has at least a bigger brother.
         //If not, give it a quick look to get how things work.
