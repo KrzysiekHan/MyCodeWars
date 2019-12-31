a3619ad8e47c9a08af50f58bfd7893baa6e9198b
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2019-12-31 23:44:00 +0100

    prime numbers

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,87 @@ namespace MyCodeWars
 {
     public static class _4KYU
 {
+        //Steps in primes
+        //The prime numbers are not regularly spaced.For example from 2 to 3 the step is 1. From 3 to 5 the step is 2. 
+        //From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2-steps primes:
+        //3, 5 - 5, 7, - 11, 13, - 17, 19, - 29, 31, - 41, 43
+        //We will write a function step with parameters:
+        //g(integer >= 2) which indicates the step we are looking for,
+        //m(integer >= 2) which gives the start of the search(m inclusive),
+        //n(integer >= m) which gives the end of the search(n inclusive)
+        //In the example above step(2, 2, 50) will return [3, 5] which is the first pair between 2 and 50 with a 2-steps.
+        //So this function should return the first pair of the two prime numbers spaced with a step of g between the limits m, n 
+        //if these g-steps prime numbers exist otherwise nil or null or None or Nothing or[] or "0, 0" or {0, 0} or 0 0(depending on the language).
+        //#Examples:
+        //step(2, 5, 7) --> [5, 7] or(5, 7) or {5, 7} or "5 7"
+        //step(2, 5, 5) --> nil or ... or[] in Ocaml or {0, 0} in C++
+        //step(4, 130, 200) --> [163, 167] or(163, 167) or {163, 167}
+        //See more examples for your language in "RUN"
+        //Remarks:
+        //([193, 197] is also such a 2-steps primes between 130 and 200 but it's not the first pair).
+        //step(6, 100, 110) --> [101, 107] though there is a prime between 101 and 107 which is 103; the pair 101-103 is a 2-step.
+        //#Notes: The idea of "step" is close to that of "gap" but it is not exactly the same. 
+        //For those interested they can have a look at http://mathworld.wolfram.com/PrimeGaps.html.
+        //A "gap" is more restrictive: there must be no primes in between(101-107 is a "step" but not a "gap". Next kata will be about "gaps":-).
+        /*
+        public static long[] Step(int g, long m, long n)
+        {
+            for (long i = m; i <= n; i++)
+            {
+                if (isPrime(i))
+                {
+                    if (isPrime(i + g) && (i + g) <= n)
+                    {
+                        return new long[2] { i, i + g };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static bool isPrime(long number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false; //Even number     
+            for (int i = 3; i <= Math.Ceiling(Math.Sqrt(number)); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }  
+         */
+        public static long[] Step(int g, long m, long n)
+        {
+            List<long> primes = new List<long>();
+            long stno = m;
+            long enno = n;
+            long num,ctr,i = 0;
+            num = ctr = i = 0;
+            for (num = stno; num <= enno; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+                if (ctr == 0 && num != 1) primes.Add(num);
+            }
+            for (int j = 0; j < primes.Count-1; j++)
+            {
+                for (int k = 0; k < primes.Count; k++)
+                {
+                    if (primes[k] - primes[j] == g) return new long[2] { primes[j], primes[k] };
+                }            
+            }
+            return new long[] { };
+        }
+
         //Salesman's Travel
         //A traveling salesman has to visit clients.He got each client's address e.g. "432 Main Long Road St. Louisville OH 43071" as a list.
         //The basic zipcode format usually consists of two capital letters followed by a white space and five digits.The list of 
@@ -28,19 +109,12 @@ namespace MyCodeWars
         //travel(r, "NY 5643") --> "NY 5643:/"
         public static string Travel(string r, string zipcode)
         {
-            Regex regex = new Regex(@"\s"+zipcode+"$");
-            var tessst = r.Split(',')
-             .Where(x => regex.IsMatch(x));
-
-            Console.WriteLine(r);
-            Console.WriteLine(zipcode);
-            Console.WriteLine("------");
-            
             if (string.IsNullOrEmpty(zipcode)) return "empty";
             if (!r.Contains(zipcode))
             {
                 return "zipcode:/";
             }
+            Regex regex = new Regex(@"\s" + zipcode + "$");
             var zips = r.Split(',')
              .Where(x => regex.IsMatch(x))
              .Select( x => x.Replace(zipcode, ""))
