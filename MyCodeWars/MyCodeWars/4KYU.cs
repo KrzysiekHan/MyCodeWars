43a55fe7df7591c89590c7b5e13c6ff52bf71e92
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-01-02 21:15:04 +0100

    backwards prime

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,52 @@ namespace MyCodeWars
 {
     public static class _4KYU
 {
+        //Backwards Read Primes
+        //Backwards Read Primes are primes that when read backwards in base 10 (from right to left) are a different prime. 
+        //(This rules out primes which are palindromes.)
+        //Examples:
+        //13 17 31 37 71 73 are Backwards Read Primes
+        //13 is such because it's prime and read from right to left writes 31 which is prime too. Same for the others.
+        //Task
+        //Find all Backwards Read Primes between two positive given numbers(both inclusive), the second one always being 
+        //greater than or equal to the first one.The resulting array or the resulting string will be ordered following the natural order of the prime numbers.
+        //Example
+        //backwardsPrime(2, 100) => [13, 17, 31, 37, 71, 73, 79, 97] backwardsPrime(9900, 10000) => [9923, 9931, 9941, 9967] backwardsPrime(501, 599) => []
+        //Note for Forth
+        //Return only the first backwards-read prime between start and end or 0 if you don't find any
+        //backwardsPrime(2, 100) => "13 17 31 37 71 73 79 97"
+        //backwardsPrime(9900, 10000) => "9923 9931 9941 9967"
+        public static string backwardsPrime(long start, long end)
+        {
+            List<long> primes = new List<long>();
+            long stno = start;
+            long enno = end;
+            long num, ctr, i = 0;
+            num = ctr = i = 0;
+            for (num = stno; num <= enno; num++)
+            {
+                ctr = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+                if (ctr == 0 && num != 1) primes.Add(num);
+            }
+
+            foreach (var item in primes)
+            {
+                var itemval = item.ToString().ToCharArray().Select(x => char.GetNumericValue(x)).Sum();
+                primes.Where(x => x.ToString().ToCharArray().Select(z => char.GetNumericValue(z)).Sum() == itemval).ToList();
+            }
+            return "";
+        }
+
+
         //Sum consecutives
         //You are given a list/array which contains only integers(positive and negative). Your job is to sum only
         //the numbers that are the same and consecutive.The result should be one list.
