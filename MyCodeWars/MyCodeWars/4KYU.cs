ef5a4d71a90674161b120fcb15907a98ef7940c1
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-01-07 21:36:25 +0100

    get middle

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,31 @@ namespace MyCodeWars
 {
     public static class _4KYU
 {
+        //Get the Middle Character
+        //You are going to be given a word.Your job is to return the middle character of the word. If the word's length is odd, 
+        //return the middle character. If the word's length is even, return the middle 2 characters.
+        //#Examples:
+        //Kata.getMiddle("test") should return "es"
+        //Kata.getMiddle("testing") should return "t"
+        //Kata.getMiddle("middle") should return "dd"
+        //Kata.getMiddle("A") should return "A"
+        //#Input
+        //A word(string) of length 0 < str< 1000 (In javascript you may get slightly more than 1000 in some test 
+        //cases due to an error in the test cases). You do not need to test for this. This is only here 
+        //to tell you that you do not need to worry about your solution timing out.
+        //#Output
+        //The middle character(s) of the word represented as a string.
+        public static string GetMiddle(string s)
+        {
+            if (s.Length%2==0)//even
+            {
+                return s[s.Length / 2 - 1].ToString() + s[s.Length / 2 ].ToString();
+            } else //odd
+            {
+                return s[s.Length / 2].ToString();
+            }
+        }
+
         //Break camelCase
         //Complete the solution so that the function will break up camel casing, using a space between words.
         //Example
