fd21a1f34d748339757fc065cd6d86246df65156
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-01-09 22:20:54 +0100

    ten minute walk

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -10,6 +10,48 @@ namespace MyCodeWars
 {
     public static class _4KYU
 {
+        //Take a ten minute walk
+        //You live in the city of Cartesia where all roads are laid out in a perfect grid.You arrived ten minutes 
+        //too early to an appointment, so you decided to take the opportunity to go for a short walk.The city 
+        //provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends 
+        //you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always 
+        //walk only a single block in a direction and you know it takes you one minute to traverse one city block, so 
+        //create a function that will return true if the walk the app gives you will take you exactly ten minutes (you 
+        //don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+        //Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). 
+        //It will never give you an empty array(that's not a walk, that's standing still!).
+        public static bool IsValidWalk(string[] walk)
+        {
+            int x, y;
+            x = y = 0;
+            for (int i = 0; i < walk.Length; i++)
+            {
+                walkOneBlock(walk[i], ref x, ref y);
+            }
+            return (x == 0 && y == 0) ? true : false;
+        }
+        private static void walkOneBlock (string dir,ref int x,ref int y)
+        {
+            switch (dir)
+            {
+                case "n":
+                    y++;
+                    break;
+                case "s":
+                    y--;
+                    break;
+                case "w":
+                    x--;
+                    break;
+                case "e":
+                    x++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
         //Playing on a chessboard
         //With a friend we used to play the following game on a chessboard(8, rows, 8 columns). On the first row at the bottom we put numbers:
         //1/2, 2/3, 3/4, 4/5, 5/6, 6/7, 7/8, 8/9
