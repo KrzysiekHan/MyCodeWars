a04fcee44706f787f9fa3b70e54876d7a4a38cfd
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-02-12 21:01:11 +0100

    printer errors

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -24,6 +24,24 @@ namespace MyCodeWars
     }
     public static class _4KYU
     {
+
+        //Printer Errors
+        //In a factory a printer prints labels for boxes.For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+        //The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+        //Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g.aaaxbbbbyyhwawiwjjjwwm with letters not from a to m
+        //You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+        //The string has a length greater or equal to one and contains only letters from ato z.
+        //#Examples:
+        //s= "aaabbbbhaijjjm"
+        //error_printer(s) => "0/14"
+        //s= "aaaxbbbbyyhwawiwjjjwwm"
+        //error_printer(s) => "8/22"
+        public static string PrinterError(String s)
+        {
+            return $"{s.Where(x => (int)x > (int)'m').Count()}/{s.Length}";
+        }
+
+
         //List Filtering
         //In this kata you will create a function that takes a list of non-negative integers and strings and returns a new 
         //list with the strings filtered out.
