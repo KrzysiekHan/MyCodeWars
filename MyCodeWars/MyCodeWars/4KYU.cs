e96b7241d56bd826236f3b5aaf773f9ff43679f6
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-06 21:46:30 +0100

    numerical of string

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -24,6 +24,32 @@ namespace MyCodeWars
     }
     public static class _4KYU
     {
+        //Numericals of a String
+        //You are given an input string.
+        //For each symbol in the string if it's the first character occurrence, replace it with a '1', else replace it with the amount of times you've already seen it...
+        //But will your code be performant enough?
+        //Examples:
+        //input   =  "Hello, World!"
+        //result  =  "1112111121311"
+        //input   =  "aaaaaaaaaaaa"
+        //result  =  "123456789101112"
+        //There might be some non-ascii characters in the string.
+        public static string Numericals(string s)
+        {
+            Console.WriteLine(s+Environment.NewLine);
+            int[] counts = new int[255];
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var charIndex = (int)s[i]; 
+                counts[charIndex] += 1;
+                result.Append(counts[charIndex].ToString());
+            }
+            return result.ToString();
+        }
+        
+
         //The museum of incredible dull things
         //Task
         //Given an array of integers, remove the smallest value.Do not mutate the original array/list.If there are multiple elements with the same 
