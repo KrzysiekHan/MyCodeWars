82b94feffa36c090c8e58a7901dc1d8550eceba4
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-15 20:26:35 +0100

    odd vs even

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -9,14 +9,21 @@ using System.Threading;
 
 namespace MyCodeWars
 {
-
-    public static class CustomStringExtensions
-    {
-
-    }
-
     public static class _4KYU
     {
+        //Odd or even
+        //Given a list of numbers, determine whether the sum of its elements is odd or even.
+        //Give your answer as a string matching "odd" or "even".
+        //If the input array is empty consider it as: [0] (array with a zero).
+        //Example:
+        //odd_or_even([0])          ==  "even"
+        //odd_or_even([0, 1, 4])    ==  "odd"
+        //odd_or_even([0, -1, -5])  ==  "even"
+        public static string OddOrEven(int[] array)
+        {
+            return array.Sum() % 2 == 0 ? "even" : "odd";
+        }
+
         //Find the divisors!
         //Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors
         //(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' 
