6173c0fecd261098568ba1068f0aa9547927cca6
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-17 20:56:43 +0100

    sum of first nth term

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,27 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Sum of the first nth term of Series
+        //Your task is to write a function which returns the sum of following series upto nth term(parameter).
+        //Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+        //Rules:
+        //You need to round the answer to 2 decimal places and return it as String.
+        //If the given value is 0 then it should return 0.00
+        //You will only be given Natural Numbers as arguments.
+        //Examples:
+        //SeriesSum(1) => 1 = "1.00"
+        //SeriesSum(2) => 1 + 1 / 4 = "1.25"
+        //SeriesSum(5) => 1 + 1 / 4 + 1 / 7 + 1 / 10 + 1 / 13 = "1.57"
+        public static string seriesSum(int n)
+        {
+            decimal result = 0M;
+            for (int i = 0; i < n; i++)
+            {
+                result += 1 / ((decimal)i * 3 + 1);
+            }
+            return Math.Round(result, 2).ToString();
+        }
+
         //Don't give me five!
         //In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it.
         //The start and the end number are both inclusive!
