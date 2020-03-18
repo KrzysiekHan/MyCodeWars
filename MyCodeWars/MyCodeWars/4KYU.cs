0c785488adf780c73bbf20b0e41d12eb22e833f8
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-18 20:28:36 +0100

    stray

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,19 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+
+        //Find the stray number
+        //You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+        //Complete the method which accepts such an array, and returns that single different number.
+        //The input array will always be valid! (odd-length >= 3)
+        //Examples
+        //[1, 1, 2] ==> 2
+        //[17, 17, 3, 17, 17, 17, 17] ==> 3
+        public static int Stray(int[] numbers)
+        {
+            return 0;
+        }
+
         //Sum of the first nth term of Series
         //Your task is to write a function which returns the sum of following series upto nth term(parameter).
         //Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
