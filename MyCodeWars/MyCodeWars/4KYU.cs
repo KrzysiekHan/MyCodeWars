d36a1748615a2ce99439a2be03f9c44868e4e8c6
Krzysztof Handzlik <krzysztofhandzlik89@gmail.com>
2020-03-23 20:45:39 +0100

    max profit

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,25 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //The highest profit wins!
+        //Story
+        //Ben has a very simple idea to make some profit: he buys something and sells it again.Of course, this wouldn't give him any profit at 
+        //all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+        //Task
+        //Write a function that returns both the minimum and maximum number of the given list/array.
+        //Examples
+        //MinMax.minMax(new int[]{1,2,3,4,5}) == {1,5}
+        //MinMax.minMax(new int[]{2334454,5}) == {5, 2334454}
+        //MinMax.minMax(new int[]{1}) == {1, 1}
+        //Remarks
+        //All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an 
+        //array or a list, you don't have to check for null, undefined or similar.
+        public static int[] minMax(int[] lst)
+        {
+            var a = lst.Min();
+            var b = lst.Max();
+            return new int[] { a, b };
+        }
 
         //Is this a triangle?
         //Implement a method that accepts 3 integer values a, b, c.The method should return true if a triangle can be built
