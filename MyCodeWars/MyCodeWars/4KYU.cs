9fde1603befbe2b18ff770524ed00b6c6fd9ff9d
k.handzlik <k.handzlik@infomex.pl>
2020-04-08 22:25:38 +0200

    responsible drinking

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,34 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Responsible Drinking
+        //Welcome to the Codewars Bar
+        //Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+        //Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting 
+        //how many glasses of water you should drink to not be hungover.
+        //Example parties:
+        //Input 0:
+        //"1 beer"
+        //Output 0:
+        //"1 glass of water"
+        //Explaination 0:
+        //You drank one standard drink
+        //Input 1:
+        //"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"
+        //Output 1:
+        //"10 glasses of water"
+        //Explaination 1:
+        //You drank ten standard drinks
+        //Note:
+        //To keep the things simple, we'll considere that any "numbered thing" in the string is a drink. 
+        //Even "1 bear" => "1 glass of water" or "1 chainsaw and 2 pools" => "3 glasses of water"...
+        public static string hydrate(string drinkString)
+        {
+            var glasses = drinkString.Split(' ').Select(x => int.TryParse(x, out _) ? int.Parse(x) : 0).Sum();
+            string multiple = "";
+            if (glasses > 1) multiple = "es";
+            return glasses + "glass" + multiple + " of water";
+        }
 
         //Enumerable Magic #5- True for Just One?
         //Create a function called one that accepts two params:
