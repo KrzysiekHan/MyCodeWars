4da400df5ee33ac565e87f1febb2d1988352955d
k.handzlik <k.handzlik@infomex.pl>
2020-04-11 11:30:30 +0200

    ends with

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,15 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //String ends with?
+        //Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument(also a string).
+        //Examples:
+        //solution('abc', 'bc') // returns true
+        //solution('abc', 'd') // returns false
+        public static bool Solution(string str, string ending)
+        {
+            return Regex.IsMatch(str, ".*" + ending + "$");
+        }
 
         //Most digits
         //Find the number with the most digits.
