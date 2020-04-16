f92b39c8aa8c7d0eef3a0edf8ed667be925adc2f
k.handzlik <k.handzlik@infomex.pl>
2020-04-16 21:09:59 +0200

    middle element

--- MyCodeWars/MyCodeWars/4KYU.cs
+++ MyCodeWars/MyCodeWars/4KYU.cs
@@ -11,6 +11,22 @@ namespace MyCodeWars
 {
     public static class _4KYU
     {
+        //Find the middle element
+        //As a part of this Kata, you need to create a function that when provided with a triplet, 
+        //returns the index of the numerical element that lies between the other two elements.
+        //The input to the function will be an array of three distinct numbers
+        //For example:
+        //gimme([2, 3, 1]) => 0
+        //2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+        //Another example(just to make sure it is clear):
+        //gimme([5, 10, 14]) => 1
+        //10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+        public static int Gimme(double[] inputArray)
+        {
+            var temp = inputArray.OrderBy(x => x).ToArray();
+            return inputArray.ToList().IndexOf(temp[1]);
+        }
+
         //Sort array by string length
         //Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
         //For example, if this array were passed as an argument:
